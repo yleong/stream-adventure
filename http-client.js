b4750fc480e9b5c775b401e1dd31e8a0fea6a5bc
Yew Long Leong <yew@cloudflare.com>
2023-11-23 18:28:20 +0800

    add http-client

---
+++ http-client.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { request } = require('http')
+
+const options = { method: 'POST' }
+const req = request('http://localhost:8099', options, (res) => {
+   res.pipe(process.stdout) 
+})
+process.stdin.pipe(req)
