81142048dff610a439598f6d00b21a189c58df1b
Yew Long Leong <yew@cloudflare.com>
2023-12-02 16:51:45 +0800

    add duplexer-redux

---
+++ duplexer-redux.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const duplexer2 = require('duplexer2')
+const { Writable } = require('stream');
+
+module.exports = function (counter) {
+    let occurrences = {}
+
+    // Custom Writable stream in object mode
+    const myWritableStream = new Writable({
+        objectMode: true,
+        write(chunk, encoding, callback) {
+            // Handle the written chunk
+            let cc = chunk.country
+            if (occurrences.hasOwnProperty(cc)) {
+                occurrences[cc] += 1
+            } else {
+                occurrences[cc] = 1
+            }
+            callback();
+        },
+    });
+    myWritableStream.on('finish', () => {
+        counter.setCounts(occurrences)
+    });
+
+    process.stdin.pipe(myWritableStream);
+
+    return duplexer2({objectMode: true}, myWritableStream, counter)
+}
